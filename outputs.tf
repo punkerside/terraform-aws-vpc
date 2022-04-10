8a24ca0c066b08ca56524fecd5edca4ec5d3986d
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2022-04-09 23:44:22 -0500

    descomentando salidas

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-# output "vpc" {
-#   description = "VPC values"
-#   value       = aws_vpc.this
-# }
+output "vpc" {
+  description = "VPC values"
+  value       = aws_vpc.this
+}
 
-# output "subnet_private_ids" {
-#   description = "Subnet private ids"
-#   value       = aws_subnet.private.*.id
-# }
+output "subnet_private_ids" {
+  description = "Subnet private ids"
+  value       = aws_subnet.private.*.id
+}
 
-# output "subnet_public_ids" {
-#   description = "Subnet public ids"
-#   value       = aws_subnet.public.*.id
-# }
+output "subnet_public_ids" {
+  description = "Subnet public ids"
+  value       = aws_subnet.public.*.id
+}
