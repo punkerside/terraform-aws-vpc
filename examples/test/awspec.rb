6dd8287ff0515a8789f36f2506a6526144ea1c31
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2022-10-01 00:52:20 -0500

    multiples mejoreas

--- examples/test/awspec.rb
+++ examples/test/awspec.rb
@@ -3,10 +3,6 @@ require 'aws-sdk'
 
 vpc_name = `cd examples/test/ && terraform output name`.strip.delete('"')
 
-ec2 = Aws::EC2::Client.new()
-azs = ec2.describe_availability_zones
-zone_names = azs.to_h[:availability_zones].first(2).map { |az| az[:zone_name] }
-
 describe vpc(vpc_name.to_s) do
   it { should exist }
   it { should be_available }
